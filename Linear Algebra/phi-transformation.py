82ebb703b12dd7796605d56d1abdcf8b3d2ccfb1
Masao Ishihara <97398246+masaoIsh@users.noreply.github.com>
2026-01-19 11:30:43 +0900

    Add phi transformation

---
+++ Linear Algebra/phi-transformation.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def phi_transform(data: list[float], degree: int) -> list[list[float]]:
+	"""
+	Perform a Phi Transformation to map input features into a higher-dimensional space by generating polynomial features.
+
+	Args:
+		data (list[float]): A list of numerical values to transform.
+		degree (int): The degree of the polynomial expansion.
+
+	"""
+	# Your code here
+	output = []
+    for element in data:
+        output.append([element**i for i in range(degree+1)])
+    
+    return output
