4665929f806008ee5d846e2bfec4e78cb4628180
Masao Ishihara <97398246+masaoIsh@users.noreply.github.com>
2026-03-14 18:01:05 +0900

    Add conditional probability from joint distribution calculator

---
+++ Probability and Statistics/conditional-probability-joint-dist.py
@@ -0,0 +1,16 @@
+def conditional_probability(joint_distribution: dict) -> float:
+	"""
+	Compute conditional probability P(A|B) from a joint probability distribution.
+
+	Args:
+		joint_distribution (dict): dictionary with keys ('A','B'), ('A','`B'), ('`A','B'), ('`A','`B')
+
+	Returns:
+		float: Conditional probability P(A|B)
+	"""
+	# Your code here
+	p_b = joint_distribution[('A', 'B')] + joint_distribution[('`A', 'B')]
+	p_a_and_b = joint_distribution[('A', 'B')]
+	val = p_a_and_b / p_b
+
+	return val
